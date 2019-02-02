8a0f14fa6af65bf261507b90dec7f3f587c7a510
Jean Paul Marinho <jean.marinho@ibm.com>
2019-02-02 18:19:15 -0200

    Initial Commit

---
+++ MogiHack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MogiHack
+//
+//  Created by jeanpaul on 2/2/19.
+//  Copyright © 2019 Jean Paul Marinho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MogiHack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MogiHack
+//
+//  Created by jeanpaul on 2/2/19.
+//  Copyright © 2019 Jean Paul Marinho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
