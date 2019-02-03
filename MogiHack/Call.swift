8cbc9ddf6904ba09cb757d838f4cbd154065efa4
Jean Paul Marinho <jean.marinho@ibm.com>
2019-02-02 22:04:11 -0200

    Some changes

---
+++ MogiHack/Call.swift
@@ -0,0 +1,12 @@
+//
+//  Call.swift
+//  MogiHack
+//
+//  Created by jeanpaul on 2/2/19.
+//  Copyright © 2019 Jean Paul Marinho. All rights reserved.
+//
+
+import Foundation
+
+struct Call: Codable {
+}
---
+++ MogiHack/OperatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OperatorViewController.swift
+//  MogiHack
+//
+//  Created by jeanpaul on 2/2/19.
+//  Copyright © 2019 Jean Paul Marinho. All rights reserved.
+//
+
+import Cocoa
+
+class OperatorViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
